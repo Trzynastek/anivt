fa30f368e2a49f8e3e02c28e42bae402b5888f94
trzynastek <trzynu@proton.me>
2026-04-13 17:04:55 +0200

    Modified matching conditions to prevent false positives.

--- modules/watcher.py
+++ modules/watcher.py
@@ -180,7 +180,7 @@ class instance:
 
                         title = title.replace("'", '’')
 
-                        if fuzz.ratio(normItem, normTitle) >= 80:
+                        if fuzz.ratio(normItem, normTitle) >= 80 and fuzz.token_sort_ratio(normItem, normTitle) >= 80:
                             var.console.debug('Title found.', variables={
                                 'title': title,
                                 'normItem': normItem,
