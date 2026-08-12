1a471d76cce679a2d0086d96d4e018a23e8791db
trzynastek <trzynu@proton.me>
2026-08-12 21:54:13 +0200

    Fixed a mistake in ffmpeg argument naming.

--- modules/downloader.py
+++ modules/downloader.py
@@ -296,7 +296,7 @@ class instance():
                 )
                 .output(
                     out,
-                    vf_filters = f"vpp_qsv=format=nv12,hwdownload,format=nv12,subtitles={fsub}{f',{vf_extra}' if vf_extra else ''},hwupload,format=qsv",
+                    vf = f"vpp_qsv=format=nv12,hwdownload,format=nv12,subtitles={fsub}{f',{vf_extra}' if vf_extra else ''},hwupload,format=qsv",
                     movflags="+faststart",
                     map=mappings,
                     **var.config['quicksync_options']
